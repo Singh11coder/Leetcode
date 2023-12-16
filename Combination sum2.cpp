6b7d446bca3dad6063a5980047a22aebab708d3c
Sukhminder Singh <143903722+Singh11coder@users.noreply.github.com>
2023-12-16 23:28:21 +0530

    Create Combination sum2.cpp

---
+++ Combination sum2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+void find(int ind,int target,vector<int>& candidates,vector<int>& ds,vector<vector<int>>& ans)
+{
+   
+       if(target==0)
+        {
+        ans.push_back(ds);
+         return;
+        }
+       
+    
+    for(int i=ind;i<candidates.size();i++)
+    {
+        if(i>ind && candidates[i]==candidates[i-1])continue;
+        if(candidates[i]>target)break;
+        ds.push_back(candidates[i]);
+        find(i+1,target-candidates[i],candidates,ds,ans);
+        ds.pop_back();
+    }
+}
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
+    {
+        vector<int>ds;
+        vector<vector<int>>ans;
+        sort(candidates.begin(),candidates.end());
+        find(0,target,candidates,ds,ans);
+        return ans;
+    }
+};
